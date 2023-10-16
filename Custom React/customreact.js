3a9e198f4fb44c65a535afa493637af79899c4ea
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-10-16 13:13:02 +0530

    Second Way of Rendering

--- Custom React/customreact.js
+++ Custom React/customreact.js
@@ -1,18 +1,28 @@
-function customRender(reactElement, container){
-    const domElement=document.createElement(reactElement.type);
-    domElement.innerHTML=reactElement.children;
-    domElement.setAttribute('href', reactElement.props.href);
-    domElement.setAttribute('target', reactElement.props.target);
+function customRender(reactElement, container) {
+    // const domElement=document.createElement(reactElement.type);
+    // domElement.innerHTML=reactElement.children;
+    // domElement.setAttribute('href', reactElement.props.href);
+    // domElement.setAttribute('target', reactElement.props.target);
+    // container.appendChild(domElement);
+
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children;
+    for (const prop in reactElement.props) {
+        if (prop == 'childen') {
+            continue;
+        }
+        domElement.setAttribute(prop, reactElement.props[prop]);
+    }
     container.appendChild(domElement);
 }
 
-const reactElement={
+const reactElement = {
     type: 'a',
-    props:{
+    props: {
         href: 'https://google.com',
         target: '_blank'
     },
     children: 'Click me to visit google'
 }
-const mainContainer=document.querySelector('#root');
+const mainContainer = document.querySelector('#root');
 customRender(reactElement, mainContainer);
